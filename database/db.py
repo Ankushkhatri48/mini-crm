b82baf57cb7f43f6473d574955979963e06ef4f8
Ankushkhatri48 <khatriankush792@gmail.com>
2026-06-15 07:54:10 +0530

    Updated database configuration for Aiven MySQL

--- database/db.py
+++ database/db.py
@@ -1,3 +1,39 @@
+# import os
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# from database.models import Base
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# try:
+#     import streamlit as st
+#     DATABASE_URL = st.secrets["DATABASE_URL"]
+# except Exception:
+#     DATABASE_URL = os.getenv(
+#         "DATABASE_URL",
+#         "mysql+pymysql://root:password@localhost:3306/mini_crm"
+#     )
+
+# print("Database connection loaded")
+
+# engine = create_engine(
+#     DATABASE_URL,
+#     pool_pre_ping=True
+# )
+
+# SessionLocal = sessionmaker(
+#     autocommit=False,
+#     autoflush=False,
+#     bind=engine
+# )
+
+# def init_db():
+#     Base.metadata.create_all(bind=engine)
+
+# def get_session():
+#     return SessionLocal()
+
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -15,11 +51,12 @@ except Exception:
         "mysql+pymysql://root:password@localhost:3306/mini_crm"
     )
 
-print("DATABASE_URL =", DATABASE_URL)
+print("Database configuration loaded")
 
 engine = create_engine(
     DATABASE_URL,
-    pool_pre_ping=True
+    pool_pre_ping=True,
+    pool_recycle=3600
 )
 
 SessionLocal = sessionmaker(
